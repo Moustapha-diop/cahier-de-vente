473cdfaad4fb9585051ac232f45ecc336a8467af
moustapha-diop <moistaphadiop57@gmail.com>
2026-08-23 02:49:41 +0000

    Configure production apiUrl to Render

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://sales-backend-magasin.onrender.com/api/ventes' // Replace with your Render URL
+  apiUrl: 'https://cahier-de-vente.onrender.com/api/ventes'
 };
